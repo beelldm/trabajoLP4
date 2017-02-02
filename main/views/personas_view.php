d1dd75dcb90ce31e1c8e1136795537a7395dd0b7
Alumno <Alumno@192.168.35.96>
2017-02-02 17:53:01 -0500

    BEELL_TABLA

--- main/views/personas_view.php
+++ main/views/personas_view.php
@@ -9,10 +9,28 @@
             print "------------ LISTA DE ALUMNOS ------------".'<br>';
             foreach ($datos as $dato) {
                 echo '<br>';
-                print $dato["codigo"].'<br>';
-                print $dato["nombre"]."     ";
-                print $dato["apellido"]."<br/>";
-            }
+                echo "<table>
+                        <div>
+                            <thead>
+                                <tr>
+                                    <td>Codigo</td>
+                                    <td>Nombre</td>
+                                    <td>Apellido</td>
+                                </tr>
+                            </thead>
+                        <div>
+                      <tbody>";
+  
+         echo "<div class='contenido'><tr>
+                <td><input size=25 id='codigo"."'value='".$dato['codigo']."' disabled></td>
+                <td><input size=25 id='nombre"."'value='".$dato['nombre']."' ></td>
+                <td><input size=25 id='apellido"."'value='".$dato['apellido']."' ></td>
+                <td><button class='btnModificar' id='"."'>Modificar</button></td>
+                <td><button class='btnEliminar' id='"."'>Eliminar</button></td>
+               </tr></div>";
+       }
+                echo "</tbody></table>";
+    
        ?>
                     
         <?php
